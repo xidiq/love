bf1c2cbf8ffdef06d00c4af5e5207dbdbf037f04
xidiq <81724851+xidiq@users.noreply.github.com>
2022-06-09 10:56:44 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+        <h2><a href="<?php echo "www/lizmap/lizmap/www/index.php" ?>">The link to the file</a></h2>
+        <h3><a href="<?php echo "www/lizmap/lizmap/www/index2.php" ?>">The link to the file</a></h3>
+</body>
+</html>
